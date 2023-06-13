1a0915d0217db2bccc40a8a51821aaf3a9ee9fd2
Fazel-Hosseinzadeh <fazelhoseinzadeh@yahoo.com>
2023-06-13 12:04:52 +0200

    Replace a snip to get user age with a function

--- run.py
+++ run.py
@@ -3,36 +3,34 @@ def main():
     Getting user's name and age and starting (or restarting) and
       finishing the game 
     """
+    print("Welcome to HANGMAN game!")
     user_name=get_user_name()
-    #Because the input is always string in case of getting numbers should be wrap in int()
-    user_age=int(input("Please let me know how old are you?"))
-    if user_age>6:
-        print("you are eligible to play this game")
-        play_game=input("Do you want to play game (Y or N)?")
-        if play_game.lower()=='y':
-            #run function to start game
-            pass
-        else:
-            #exit
-            exit(user_name)
-    else:
-        print("you are NOT eligible to play this game")
-        exit(user_name)
+    print(f"User name is : {user_name}")
+    user_age=get_user_age()
+    print(f"User age is : {user_age}")
+        
 
 def get_user_name():
     """
-    get a name from user and check if all are alphabetic
+    Get a name from user and check if all are alphabetic
     """
-    print("Welcome to HANGMAN game!")
     valid=False
     while (not valid):
         user_name=input("What is your name? (Just alphabetic)")
         if user_name.isalpha():
             print(f"Happy to have you here {user_name}!")
             valid=True
-            return user_name
         else:
             print("The name which you inserted \nhas non-alphabetic characters.\nPlease try again!")
+    return user_name
+
+
+def get_user_age():
+    """
+    Get the age and check if all is digit and then ckech the eligibility
+    """
+    user_age=input("Pleas let me know your age? (Just number)")
+    return user_age
 
 
 def exit(user):
