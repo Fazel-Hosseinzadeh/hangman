15796324ff90114a47993de6835a50c443f5bf55
Fazel-Hosseinzadeh <fazelhoseinzadeh@yahoo.com>
2023-06-13 10:59:29 +0200

    Add Goodbye msg ti user when exiting

--- run.py
+++ run.py
@@ -11,15 +11,16 @@ def main():
     user_age=int(input("Please let me know how old are you?"))
     if user_age>6:
         print("you are eligible to play this game")
-        Play_game=input("Do you want to play game (Y or N)?")
-        if Play_game.lower()=='y':
+        play_game=input("Do you want to play game (Y or N)?")
+        if play_game.lower()=='y':
             #run function to start game
+            pass
         else:
             #exit
-            raise SystemExit(f"\nExit the Game.\n")
+            raise SystemExit(f"\nGoodbye {user_name}\nExit the Game.\n")
     else:
         print("you are NOT eligible to play this game")
-        raise SystemExit(f"\nExit the Game\n")
+        raise SystemExit(f"\nGoodbye {user_name}\nExit the Game.\n")
 
 
         
