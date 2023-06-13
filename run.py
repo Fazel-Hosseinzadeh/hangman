0453c0159a4c8649975af0a4b19a7e0d21ae2f16
Fazel-Hosseinzadeh <fazelhoseinzadeh@yahoo.com>
2023-06-13 09:26:42 +0200

    Adding main function and asking user name

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-print("Hi!")
+def main():
+    """
+    Getting user's name and age and starting (or restarting) and
+      finishing the game 
+    """
+    print("Welcome to HANGMAN game!")
+    user_name=input("What is your name?")
+    print(f"Happy to have you here {user_name}!")
+        
+main()
